a6ee3af4f7943940e04c38150cc5f4fabe055b63
breakpoint25 <breakpoint@gmail.com>
2018-03-30 15:48:22 -0700

    added google auth flow to server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
-const express = require('express')
-const cors = require('cors')
 const path = require('path');
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 const moment = require('moment')
+const keys = require('./config/keys')
+require('./models/User')
+require('./services/passport')
+
+mongoose.connect(keys.mongoURI)
 
 const app = express()
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
 let corsOption = {
   origin: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 }
-
 app.use(cors(corsOption))
 
 // Rest API requirements
@@ -156,7 +172,8 @@ getCoinList().then(() => {
 
     // Check if coin is trading, if not then skip getting price
     // since it will only return an error
-    if (data['coinList']['coins'][fsym]['IsTrading']) {
+    if (data['coinList']['coins'][fsym]
+      && data['coinList']['coins'][fsym]['IsTrading']) {
       getPrices(fsym)
     }
 
@@ -263,4 +280,7 @@ app.get('/coin/:coin', (req, res) => {
   }
 })
 
-app.listen(3001)
+require('./routes/authRoutes')(app)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
