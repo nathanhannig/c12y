dbcfa4866866a6cadac4e3fbc7d0d200aece1728
breakpoint25 <breakpoint@gmail.com>
2018-05-04 13:03:15 -0700

    fixed defect loading files for production routes

--- server/routes/productionRoutes.js
+++ server/routes/productionRoutes.js
@@ -4,10 +4,10 @@ const express = require('express')
 module.exports = (app) => {
   if (process.env.NODE_ENV === 'production') {
     // Serves the React app assets in production
-    app.use(express.static(path.join(__dirname, '/../client/build')))
+    app.use(express.static(path.join(__dirname, '/../../client/build')))
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'))
+      res.sendFile(path.join(__dirname, '../../client/build/index.html'))
     })
   }
 }
