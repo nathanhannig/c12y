95f9c12c019247f04d42ce9ed25cbdcb4fa12c12
breakpoint25 <breakpoint@gmail.com>
2018-06-04 14:20:42 -0700

    css changes, changes to metrics and search box, lowered total 24h vol sort

--- client/src/components/Metrics/index.js
+++ client/src/components/Metrics/index.js
@@ -1,9 +1,8 @@
 // React
-import React, { Component } from 'react'
+import React from 'react'
 // import { Row, Col } from 'react-bootstrap'
 import Row from 'react-bootstrap/lib/Row'
 import Col from 'react-bootstrap/lib/Col'
-import { Typeahead } from 'react-bootstrap-typeahead'
 import 'react-bootstrap-typeahead/css/Typeahead.css'
 import 'react-bootstrap-typeahead/css/Typeahead-bs4.css'
 import PropTypes from 'prop-types'
@@ -13,91 +12,70 @@ import { withRouter } from 'react-router-dom'
 import API from '../../utils'
 import './index.css'
 
-class Metrics extends Component {
-  state = {}
+const Metrics = (props) => {
+  const { coins } = props
 
-  renderSearchBox = () => {
-    const { coins } = this.props
-    let options = ['']
-
-    if (coins && coins.coinList) {
-      options = coins.coinList
-    }
-
-    return (
-      <div className="search">
-        <Typeahead
-          placeholder="Search"
-          onChange={(selected) => {
-            this.setState({ selected })
-            this.props.history.push(`/${selected[0].id}`)
-          }}
-          options={options}
-          selected={this.state.selected}
-        />
-      </div>
-    )
-  }
-
-  render() {
-    const { coins } = this.props
-
-    const totalMarketCap = coins.totalMarketCap &&
+  const totalMarketCap = coins.totalMarketCap &&
       API.formatDollarsWholeNumber(coins.totalMarketCap)
 
-    const totalVolume24h = coins.totalVolume24h &&
+  const totalVolume24h = coins.totalVolume24h &&
       API.formatDollarsWholeNumber(coins.totalVolume24h)
 
-    const btcDominance = coins.btcDominance &&
+  const totalCoins = coins.coinList &&
+  API.formatWholeNumber(coins.coinList.length)
+
+  const btcDominance = coins.btcDominance &&
       API.formatPercent(coins.btcDominance)
 
-
-    return (
-      <div className="metrics">
-        <Row>
-          <Col xs={6} md={6}>
-            <Row>
-              <Col xs={12} md={6}>
-                <Row>
-                  <Col className="header" xs={12}>Market Cap</Col>
-                  <Col className="value" xs={12}>
-                    {totalMarketCap}
-                  </Col>
-                </Row>
-              </Col>
-              <Col xs={12} md={6}>
-                <Row>
-                  <Col className="header" xs={12}>24H Volume</Col>
-                  <Col className="value" xs={12}>
-                    {totalVolume24h}
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Col>
-          <Col xs={6} md={6}>
-            <Row>
-              <Col xs={12} md={6}>
-                <Row>
-                  <Col className="header" xs={12}>BTC Dominance</Col>
-                  <Col className="value" xs={12}>
-                    {btcDominance}
-                  </Col>
-                </Row>
-              </Col>
-              <Col xs={12} md={6}>
-                {this.renderSearchBox()}
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </div>
-    )
-  }
+  return (
+    <div className="metrics">
+      <Row>
+        <Col xs={6} md={6}>
+          <Row>
+            <Col xs={12} md={6}>
+              <Row>
+                <Col className="header" xs={12}>Market Cap</Col>
+                <Col className="value" xs={12}>
+                  {totalMarketCap}
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row>
+                <Col className="header" xs={12}>24H Volume</Col>
+                <Col className="value" xs={12}>
+                  {totalVolume24h}
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={6} md={6}>
+          <Row>
+            <Col xs={12} md={6}>
+              <Row>
+                <Col className="header" xs={12}>Total Coins</Col>
+                <Col className="value" xs={12}>
+                  {totalCoins}
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row>
+                <Col className="header" xs={12}>BTC Dominance</Col>
+                <Col className="value" xs={12}>
+                  {btcDominance}
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  )
 }
 
 Metrics.propTypes = {
-  history: PropTypes.object.isRequired,
   coins: PropTypes.object.isRequired,
 }
 
---
+++ client/src/components/Search/index.js
@@ -0,0 +1,58 @@
+// React
+import React, { Component } from 'react'
+// import { Row, Col } from 'react-bootstrap'
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
+import { Typeahead } from 'react-bootstrap-typeahead'
+import 'react-bootstrap-typeahead/css/Typeahead.css'
+import 'react-bootstrap-typeahead/css/Typeahead-bs4.css'
+import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
+
+// App
+import { IoAndroidSearch as SearchIcon } from 'react-icons/lib/io'
+import './index.css'
+
+class Search extends Component {
+  state = {}
+
+  render() {
+    const { coins } = this.props
+    let options = ['']
+
+    if (coins && coins.coinList) {
+      options = coins.coinList
+    }
+
+    return (
+      <Row>
+        <Col xs={12}>
+          <div className="search">
+            <div className="search-input">
+              <Typeahead
+                placeholder="Find a coin"
+                bsSize="large"
+                onChange={(selected) => {
+                  this.setState({ selected })
+                  this.props.history.push(`/${selected[0].id}`)
+                }}
+                options={options}
+                selected={this.state.selected}
+              />
+            </div>
+            <div className="search-icon">
+              <SearchIcon />
+            </div>
+          </div>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+Search.propTypes = {
+  history: PropTypes.object.isRequired,
+  coins: PropTypes.object.isRequired,
+}
+
+export default withRouter(Search)
