d6ccf9ff89c2e2db88df1d778e25cd12c4b0d455
breakpoint25 <breakpoint@gmail.com>
2018-06-05 09:40:53 -0700

    gitignore updates

--- server/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID: '683638424935-sbarfktebjrvacsjphnd42r89jtd05dn.apps.googleusercontent.com',
-  googleClientSecret: '4P-qbKWJafyVfdE9GX1ivLDe',
-  sendgridApiKey: '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',
-  contactFormEmail: 'breakpoint@gmail.com',
-  mongoURI: 'mongodb://admin:password@ds127899.mlab.com:27899/c12y-dev',
-  cookieKey: 'ocismwjhafsmtbercezxwqknpnicxtzsxgednd',
-}
