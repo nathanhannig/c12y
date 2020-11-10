4148f74d6294ebcea0da0859fe4e60ca3b3d4ab0
Nathan Hannig <breakpoint@gmail.com>
2020-11-10 20:51:17 +0000

    fix prod path

--- server/server.js
+++ server/server.js
@@ -57,11 +57,11 @@ app.use('/email', emailRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   // Serves the React app assets in production
-  app.use(express.static(path.join(__dirname, '/../../client/build')))
+  app.use(express.static(path.join(__dirname, '../client/build')))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'))
-  })
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  )
 }
 
 app.use(notFound)
