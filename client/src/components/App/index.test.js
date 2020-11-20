041719b427545325d6426039d8171dc9d5471f15
Nathan Hannig <breakpoint@gmail.com>
2020-11-19 17:20:40 -0800

    fix tests

--- client/src/components/App/index.test.js
+++ client/src/components/App/index.test.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from '../../store'
 import App from '.'
 
 it('renders without crashing', () => {
+  // Jest jsdom does not support windows.scrollTo so we mock it
+  window.scrollTo = jest.fn()
+
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  )
   ReactDOM.unmountComponentAtNode(div)
+
+  window.scrollTo.mockClear()
 })
--- client/src/components/withTracker/index.js
+++ client/src/components/withTracker/index.js
@@ -2,13 +2,27 @@ import React, { useEffect } from 'react'
 import ReactGA from 'react-ga'
 import PropTypes from 'prop-types'
 
-const DEFAULT_CONFIG = {
-  // Un-comment below lines to use for development debugging
-  // trackingId: '',
-  // debug: true,
-  // gaOptions: {
-  //   cookieDomain: 'none',
-  // },
+let DEFAULT_CONFIG = {}
+
+if (process.env.NODE_ENV === 'development') {
+  DEFAULT_CONFIG = {
+    ...DEFAULT_CONFIG,
+    ...{
+      debug: true,
+      gaOptions: {
+        cookieDomain: 'none',
+      },
+    },
+  }
+}
+
+if (process.env.NODE_ENV === 'test') {
+  DEFAULT_CONFIG = {
+    ...DEFAULT_CONFIG,
+    ...{
+      testMode: true,
+    },
+  }
 }
 
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID, DEFAULT_CONFIG)
