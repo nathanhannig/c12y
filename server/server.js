68522b37536522069705a8a58fc405afcaeb9eb0
Nathan Hannig <breakpoint@gmail.com>
2021-01-04 15:02:52 -0800

    Create Docker containers for development, remove React apps from being servered by Express

--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
 import 'dotenv/config.js'
-
-import path from 'path'
 import express from 'express'
 import format from 'date-fns/format/index.js'
 import logger from 'loglevel'
@@ -24,10 +22,6 @@ connectDB()
 
 const app = express()
 
-// https://nodejs.org/api/esm.html#esm_no_require_exports_module_exports_filename_dirname
-// eslint-disable-next-line no-underscore-dangle
-const __dirname = path.resolve()
-
 app.use(cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey] }))
 
 // Used to parse POST form requests to req.body
@@ -44,25 +38,15 @@ const corsOption = {
 }
 app.use(cors(corsOption))
 
-
 // ROUTES --
 app.use('/api', apiRoutes)
 app.use('/auth', authRoutes)
 app.use('/email', emailRoutes)
 
-if (process.env.NODE_ENV === 'production') {
-  // Serves the React app assets in production
-  app.use('/admin', express.static(path.join(__dirname, '../admin/build')))
-  app.get('admin/*', (req, res) => res.sendFile(path.join(__dirname, '../admin/build/index.html')))
-
-  app.use(express.static(path.join(__dirname, '../client/build')))
-  app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')))
-}
-
 app.use(notFound)
 app.use(errorHandler)
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 8080
 const server = app.listen(port, () => {
   logger.info(`${format(new Date())} - Server is running in ${process.env.NODE_ENV} mode \
 on port ${server.address().port}`)
